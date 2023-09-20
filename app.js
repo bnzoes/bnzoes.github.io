4523c2b95bce51e162c9a0824f4be4e267915a72
bnzoes <144855637+bnzoes@users.noreply.github.com>
2023-09-19 20:15:30 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,72 @@
-// Function to display all products
-function displayAllProducts(products) {
+// Global variables for pagination
+let currentPage = 1;
+const productsPerPage = 4000;
+
+// Function to paginate and display products
+function displayProducts(products, page) {
     const productGrid = document.querySelector('.product-grid');
     productGrid.innerHTML = '';
 
-    products.forEach((product) => {
+    const startIndex = (page - 1) * productsPerPage;
+    const endIndex = startIndex + productsPerPage;
+
+    for (let i = startIndex; i < endIndex && i < products.length; i++) {
+        const product = products[i];
         const productCard = createProductCard(product);
         productGrid.appendChild(productCard);
+    }
+}
+
+// Function to handle search
+function handleSearch(products, searchTerm) {
+    searchTerm = searchTerm.toLowerCase().trim();
+
+    const filteredProducts = products.filter((product) => {
+        const title = product["Lucas Spreadsheet"].toLowerCase();
+        return title.includes(searchTerm);
     });
+
+    return filteredProducts;
 }
 
 // Event listener to load products when the page loads
 document.addEventListener('DOMContentLoaded', async () => {
     const jsonData = await fetchProductsFromJSON();
-    displayAllProducts(jsonData);
-});
+    let filteredProducts = jsonData;
 
+    const searchInput = document.getElementById('search');
+    const searchButton = document.getElementById('searchButton');
+    const prevPageButton = document.getElementById('prevPage');
+    const nextPageButton = document.getElementById('nextPage');
+
+    searchButton.addEventListener('click', () => {
+        const searchTerm = searchInput.value;
+        filteredProducts = handleSearch(jsonData, searchTerm);
+        currentPage = 1;
+        displayProducts(filteredProducts, currentPage);
+    });
+
+    searchInput.addEventListener('input', () => {
+        const searchTerm = searchInput.value;
+        filteredProducts = handleSearch(jsonData, searchTerm);
+        currentPage = 1;
+        displayProducts(filteredProducts, currentPage);
+    });
+
+    prevPageButton.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            displayProducts(filteredProducts, currentPage);
+        }
+    });
+
+    nextPageButton.addEventListener('click', () => {
+        const maxPage = Math.ceil(filteredProducts.length / productsPerPage);
+        if (currentPage < maxPage) {
+            currentPage++;
+            displayProducts(filteredProducts, currentPage);
+        }
+    });
+
+    displayProducts(filteredProducts, currentPage);
+});
